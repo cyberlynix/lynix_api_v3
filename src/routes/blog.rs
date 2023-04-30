39a9e4cd2769f280887fea8582d9ebaa9192656b
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-04-30 15:49:02 -0300

    Add blog and events for lynix.ca website

--- src/routes/blog.rs
+++ src/routes/blog.rs
@@ -1,5 +1,69 @@
-use actix_web::web;
+use actix_web::{web, get, HttpResponse, post};
+use bson::{oid::ObjectId, doc};
+use futures::TryStreamExt;
+use mongodb::{Collection, Client};
+
+use crate::{models::article::Article, errors::LynixError};
+
+/* Get all articles */
+#[get("/articles")]
+async fn get_articles(db: web::Data<Client>) -> Result<HttpResponse, LynixError> {
+    let collection: Collection<Article> = db.database("lynix").collection("events");
+    let mut cursor = collection.find(None, None).await?;
+
+    let mut events: Vec<Article> = Vec::new();
+
+    while let Ok(Some(event)) = cursor.try_next().await {
+        events.push(event);
+    }
+
+    Ok(HttpResponse::Ok().json(events))
+}
+
+/* Get single article */
+#[get("/article/{id}")]
+async fn get_article_by_id(db: web::Data<Client>, path: web::Path<String>) -> Result<HttpResponse, LynixError> {
+    let collection: Collection<Article> = db.database("lynix").collection("events");
+    let id = path.into_inner();
+
+    let obj_id = ObjectId::parse_str(&id).map_err(|_| LynixError::InternalError)?;
+    let filter = doc!{"_id": obj_id};
+
+    if let Ok(Some(event)) = collection.find_one(filter, None).await {
+        Ok(HttpResponse::Ok().json(event))
+    } else {
+        Err(LynixError::NotFound)
+    }
+}
+
+/* Add Article 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/article")]
+async fn add_article() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
+
+/* Update Article 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/article/{id}")]
+async fn update_article() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
+
+/* Delete Article 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/article/{id}/del")]
+async fn delete_article() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-
+    cfg.service(get_articles);
+    cfg.service(get_article_by_id);
+    cfg.service(add_article);
+    cfg.service(update_article);
+    cfg.service(delete_article);
 }
--- src/routes/events.rs
+++ src/routes/events.rs
@@ -1,5 +1,69 @@
-use actix_web::web;
+use actix_web::{web, get, HttpResponse, post};
+use bson::{oid::ObjectId, doc};
+use futures::TryStreamExt;
+use mongodb::{Collection, Client};
+
+use crate::{models::event::Event, errors::LynixError};
+
+/* Get all events */
+#[get("/events")]
+async fn get_events(db: web::Data<Client>) -> Result<HttpResponse, LynixError> {
+    let collection: Collection<Event> = db.database("lynix").collection("events");
+    let mut cursor = collection.find(None, None).await?;
+
+    let mut events: Vec<Event> = Vec::new();
+
+    while let Ok(Some(event)) = cursor.try_next().await {
+        events.push(event);
+    }
+
+    Ok(HttpResponse::Ok().json(events))
+}
+
+/* Get single event */
+#[get("/event/{id}")]
+async fn get_event_by_id(db: web::Data<Client>, path: web::Path<String>) -> Result<HttpResponse, LynixError> {
+    let collection: Collection<Event> = db.database("lynix").collection("events");
+    let id = path.into_inner();
+
+    let obj_id = ObjectId::parse_str(&id).map_err(|_| LynixError::InternalError)?;
+    let filter = doc!{"_id": obj_id};
+
+    if let Ok(Some(event)) = collection.find_one(filter, None).await {
+        Ok(HttpResponse::Ok().json(event))
+    } else {
+        Err(LynixError::NotFound)
+    }
+}
+
+/* Add Event 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/event")]
+async fn add_event() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
+
+/* Update Event 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/event/{id}")]
+async fn update_event() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
+
+/* Delete Event 
+    ⚠️ This path is disabled due to security concerns with authentication api. ⚠️
+*/
+#[post("/event/{id}/del")]
+async fn delete_event() -> Result<HttpResponse, LynixError> {
+    Err(LynixError::Unauthorized)
+}
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-
+    cfg.service(get_events);
+    cfg.service(get_event_by_id);
+    cfg.service(add_event);
+    cfg.service(update_event);
+    cfg.service(delete_event);
 }
